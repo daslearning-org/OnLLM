a35996a4ff3bca8b2321639bdc515d49ffdbbc02
Somnath Das <somnath.das@daslearning.in>
2025-10-18 15:09:30 +0530

    let us try Gemma3-270m too on desktop

--- app/main.py
+++ app/main.py
@@ -221,7 +221,7 @@ class OnLlmApp(MDApp):
             self.popup_download_model()
             return
         self.init_onnx_sess()
-        Thread(target=self.model_sync_on_init, args=("main",), daemon=True).start()
+        Thread(target=self.model_sync_on_init, args=("develop",), daemon=True).start()
         self.root.current = "chatbot_screen"
 
     def check_model_files(self, model_name):
